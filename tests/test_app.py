d9b1a1bfcb2f2611524da39b636aefe3406c4822
siyovush <shukurovsiyovush1@gmail.com>
2026-05-26 11:44:07 +0500

    Add Shablonizator main page

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,6 @@ def test_index():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
+    html = response.data.decode("utf-8")
+    assert "Анализатор страниц" in html
+    assert "bootstrap" in html.lower()
